c3b7451d31a5472d0aae52aceed56796f92a8310
seven <lc8681@gmail.com>
2019-12-23 13:14:58 +0800

    update

--- ign_spider.py
+++ ign_spider.py
@@ -1,6 +1,5 @@
 # coding:utf-8
 
-import urllib.request
 from bs4 import BeautifulSoup
 import requests
 import re
@@ -14,7 +13,7 @@ def ign_data(pageNum):
     for i in soup.select('article'):
         dict = {}
         title = i.img['alt']
-        img = "http://www.ign.中国" + i.img['src']
+        img = i.img['src']
         score = str(re.findall(r"\d+\.?\d*", str(i.find('span')))).replace('[', '').replace(']', '').replace("'", '')
         goto_url = i.a['href']
         if score is '':
